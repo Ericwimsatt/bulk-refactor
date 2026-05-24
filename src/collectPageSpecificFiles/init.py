785f89b1dc71ac2648e10c0c7a6c01ab8f4a612c
EricWimsatt <ericwimsatt@gmail.com>
2026-05-24 14:57:21 -0700

    ds

---
+++ src/collectPageSpecificFiles/init.py
@@ -0,0 +1,5 @@
+# Collect Pages (app/lobby.tsx, app/game.tsx, etc. Can be a folder)
+# Then open component directory; for each file, find what page uses them
+# if only 1 page uses it, move the file to the page's directory if the directory exists 
+# otherwise create it and move the page file to index.tsx in the folder.
+
